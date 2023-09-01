ff1daafa8d6be82a8544cbae92dc9ab587d0f341
Maruf Hossain <hossainmaruf001@gmail.com>
2023-09-02 00:51:16 +0200

    added sort by view functionalities and hover on buttons

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+let data = [];
+const cardContainer = document.getElementById("card-container");
 const handleCategory = async () => {
   const resp = await fetch(
     "https://openapi.programming-hero.com/api/videos/categories"
@@ -8,26 +10,45 @@ const handleCategory = async () => {
   handleCategoriesName(data);
 };
 
-const handleCategoriesName = (data) => {
+let clickedButton = "";
+
+const handleCategoriesName = (datas) => {
   // console.log(data);
 
   const buttonContainer = document.getElementById("button-container");
-  for (const dataa of data) {
+  for (const data of datas) {
     const button = document.createElement("button");
-    button.innerText = dataa.category;
-    button.classList.add("btn", "m-6");
+    button.innerText = data.category;
+    button.classList.add(
+      "m-6",
+      "rounded",
+      "py-2",
+      "px-5",
+      "capitalize",
+      "hover:bg-[#FF1F3D]",
+      "hover:font-semibold",
+      "hover:text-white",
+      "text-[#252525B2]",
+      "bg-[#25252526]",
+      "font-medium"
+    );
+
     button.addEventListener("click", () => {
-      handleCards(dataa.category_id);
+      // button.classList.add("bg-red-500");
+      handleCards(data.category_id);
+      clickedButton = data.category;
     });
+
     buttonContainer.appendChild(button);
   }
 };
+
 const handleCards = async (id = 1000) => {
   const resp = await fetch(
     `https://openapi.programming-hero.com/api/videos/category/${id}`
   );
-  const data = await resp.json();
-  const cardContainer = document.getElementById("card-container");
+  data = await resp.json();
+
   // const drawingcardContainer = document.getElementById("drawing-container");
   cardContainer.innerHTML = "";
   // drawingcardContainer.innerHTML = "";
@@ -44,6 +65,7 @@ const handleCards = async (id = 1000) => {
   } else {
     cardContainer.classList.add("grid");
   }
+
   data.data.forEach((element) => {
     // console.log(element);
 
@@ -93,34 +115,67 @@ const handleCards = async (id = 1000) => {
     `;
 
     cardContainer.appendChild(div);
-    const postedDate = document.getElementById("posted-date");
-    // if (element.others.posted_date) {
-    //   postedDate.classList.add("hidden");
-    // } else {
-    //   postedDate.classList.remove("hidden");
-    // }
-    const verified = document.getElementById("verified-id");
-    // // // verified.innerText = "";
-    // if (element.authors[0].verified === true) {
-    //   console.log(element.authors[0].verified);
-    //   // console.log(element.authors[0].verified);
-    //   // verified.innerHTML = `<img src="./images/fi_10629607.svg" alt="" />`;
-    //   const image = document.createElement("img");
-    //   image.setAttribute("src", "./images/fi_10629607.svg"); // Replace with the path to your image
-    //   image.setAttribute("alt", "Verified Icon"); // Provide an alt text for accessibility
-    //   verified.appendChild(image);
-    // } else {
-    //   verified.textContent = "Not available";
-    //   console.log("false");
-    // }
-    const verifiedText = verified.innerText;
-    console.log(verifiedText);
   });
 };
 
+document.getElementById("sort-btn").addEventListener("click", () => {
+  data?.data?.sort((a, b) => {
+    return (
+      parseFloat(b.others.views.slice(0, b.others.views.length - 1)) -
+      parseFloat(a.others.views.slice(0, a.others.views.length - 1))
+    );
+  });
+  cardContainer.textContent = "";
+
+  data.data.forEach((element) => {
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <div class="card relative h-[320px] ">
+    <figure class="">
+      <img class="h-[250px] lg:h-[180px] w-full rounded-lg" src="${
+        element.thumbnail
+      }" />
+    </figure>
+    <div class="card-body   p-1">
+      <div class=" flex ">
+        <div class=" flex gap-2">
+          <div>
+            <div class="avatar">
+              <div class="w-10 rounded-full">
+                <img src="${element.authors[0].profile_picture}" />
+              </div>
+            </div>
+          </div>
+          <div class="">
+          <h3 id="posted-date" class="absolute lg:top-36 top-[200px] bg-[#171717] right-2 text-white">${
+            element.others.posted_date
+              ? Math.floor(element.others.posted_date / 3600) + "hrs"
+              : ""
+          } ${
+      element.others.posted_date
+        ? Math.floor((element.others.posted_date % 3600) / 60) + "min ago"
+        : ""
+    }</h3>
+            <h2 class="text-[16px] font-bold">${element.title}</h2>
+            <h6>${
+              element.authors[0].profile_name
+            } <span id="verified-id" class="">${
+      element.authors[0].verified
+        ? '<img class="inline" src="./images/fi_10629607.svg" alt="" />'
+        : ""
+    }</span></h6>
+            <p>${element.others.views} Views</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+  </div>
+    `;
+
+    cardContainer.appendChild(div);
+  });
+});
+
 handleCategory();
 handleCards();
-
-// ${(
-// element.others.posted_date / 3600
-// ).toFixed(0)}hrs
